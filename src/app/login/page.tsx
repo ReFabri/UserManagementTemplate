5f26ebade9a4b411dcf7a26d68ca93acaff2d6cd
Renato <renato.f.fabri@gmail.com>
2023-08-24 18:36:53 -0300

    Add frontend and backend base file structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex-1">
+      <h1 className="text-center text-white text-2xl">Login</h1>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <div className="flex-1">
+      <h1 className="text-center text-white text-2xl">signup</h1>
+    </div>
+  );
+}
