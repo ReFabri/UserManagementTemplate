a4a3d8e2a8fac31721694394b27bd37d4d6b13a7
Renato <renato.f.fabri@gmail.com>
2023-08-24 19:06:19 -0300

    Add DB connection, fix .env naming to NextJs Standard

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URL!);
+    const connection = db.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB is connected");
+    });
+    connection.on("error", (error) => {
+      console.log("MongoDB connection failed. ", error);
+      process.exit();
+    });
+
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.log("Something went wrong!");
+    console.log(error);
+  }
+}
