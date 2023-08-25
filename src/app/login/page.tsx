ce6213f992b2249eb0ce661dac490b97933621e7
Renato <renato.f.fabri@gmail.com>
2023-08-25 20:08:03 -0300

    Add login page frontend code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,60 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { axios } from "axios";
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prevState) => {
+      return { ...prevState, email: e.target.value };
+    });
+  };
+  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prevState) => {
+      return { ...prevState, password: e.target.value };
+    });
+  };
+
   return (
-    <div className="flex-1">
-      <h1 className="text-center text-white text-2xl">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={emailHandler}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={passwordHandler}
+        placeholder="password"
+      />
+
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <Link href="/signup">Signup page</Link>
     </div>
   );
 }
