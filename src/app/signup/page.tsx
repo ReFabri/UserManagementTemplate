0857d2ded9d35f59bca2d0b18c65b4fd9d9b96dd
Renato <renato.f.fabri@gmail.com>
2023-08-25 19:58:18 -0300

    Add signup page frontend code

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { axios } from "axios";
+import { useRouter } from "next/navigation";
+
 export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  const usernameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prevState) => {
+      return { ...prevState, username: e.target.value };
+    });
+  };
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prevState) => {
+      return { ...prevState, email: e.target.value };
+    });
+  };
+  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prevState) => {
+      return { ...prevState, password: e.target.value };
+    });
+  };
+
   return (
-    <div className="flex-1">
-      <h1 className="text-center text-white text-2xl">signup</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={usernameHandler}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={emailHandler}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={passwordHandler}
+        placeholder="password"
+      />
+
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onSignup}
+      >
+        Signup
+      </button>
+      <Link href="/login">Login page</Link>
     </div>
   );
 }
