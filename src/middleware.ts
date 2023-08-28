21740a1df8d41df39d63c4811a7600b546d0ffda
Renato <renato.f.fabri@gmail.com>
2023-08-28 17:57:57 -0300

    ADD middleware to redirect logged users from public pages

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const token = request.cookies.get("token")?.value || "";
+
+  const publicPaths = ["/login", "/signup"];
+
+  const isPublicPath = publicPaths.includes(path);
+
+  if (token && isPublicPath) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!token && !isPublicPath) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/profile/:id*", "/login", "/signup"],
+};
