da99b08ccebbdf34dc341a2f1297e6fbec3d2b25
Renato <renato.f.fabri@gmail.com>
2023-08-28 20:47:59 -0300

    FIX reduced nesting in user json

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -7,13 +7,7 @@ export async function GET(request: NextRequest) {
   try {
     const userId = await getTokenData(request);
     const user = await User.findById(userId).select("-password");
-    return NextResponse.json(
-      {
-        message: "User found",
-        data: user,
-      },
-      { status: 200 }
-    );
+    return NextResponse.json(user, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
