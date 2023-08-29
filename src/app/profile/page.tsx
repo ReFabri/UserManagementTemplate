9583db0035cd8ae0042a3b09b9c4c7e547b3cdd7
Renato <renato.f.fabri@gmail.com>
2023-08-28 21:13:07 -0300

    ADD profile page button to request user id

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [user, setUser] = useState<any>(null);
   const logoutHandler = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -16,18 +18,36 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/me");
+    setUser(response.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Toaster />
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {user === null ? (
+          "No user"
+        ) : (
+          <Link href={`/profile/${user}`}>Go to user profile</Link>
+        )}
+      </h2>
       <hr />
       <button
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={logoutHandler}
       >
         Logout
+      </button>
+      <button
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        Get User details
       </button>
     </div>
   );
