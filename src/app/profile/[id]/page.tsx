1f1dc653284adb06164e32bee0019e13824999bb
Renato <renato.f.fabri@gmail.com>
2023-09-07 18:40:21 -0300

    Initial changes to show user details in /profile/:id

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import FormContainer from "@/app/_components/FormContainer";
 
 export default function ProfileIdPage({ params }: any) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUserDetails = async () => {
+      const response = await axios.get("/api/users/me");
+      setUser(response.data);
+    };
+    getUserDetails();
+  }, []);
+
   return (
     <FormContainer formName="Profile">
-      <p className="text-4xl">
-        Profile page
+      <p className="text-xl mb-4">
+        Profile Id:
         <span className="p-2 ml-6 rounded bg-orange-500 text-black">
           {params.id}
         </span>
