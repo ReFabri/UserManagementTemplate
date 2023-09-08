cfc476724dac053a1d659a3da0b3c654f9322d9e
Renato <renato.f.fabri@gmail.com>
2023-09-08 16:28:43 -0300

    Changed route to detect step of email verification

--- src/app/api/users/emailverification/route.ts
+++ src/app/api/users/emailverification/route.ts
@@ -1,32 +1,42 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
 export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
-    const { token } = reqBody;
+    const { token, userData } = reqBody;
 
-    const user = await User.findOne({
-      verifyToken: token,
-      verifyTokenExpiry: { $gt: Date.now() },
-    });
+    if (userData && !token) {
+      const { email, emailType, userId } = userData;
+      await sendEmail({ email, emailType, userId });
+      return NextResponse.json(
+        { message: "Verification email sent", emailSuccess: true },
+        { status: 200 }
+      );
+    } else if (token && !userData) {
+      const user = await User.findOne({
+        verifyToken: token,
+        verifyTokenExpiry: { $gt: Date.now() },
+      });
 
-    if (!user) {
-      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+      if (!user) {
+        return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+      }
+
+      user.isVerified = true;
+      user.verifyToken = undefined;
+      user.verifyTokenExpiry = undefined;
+      await user.save();
+
+      return NextResponse.json({
+        message: "Email verified successfully",
+        success: true,
+      });
     }
-
-    user.isVerified = true;
-    user.verifyToken = undefined;
-    user.verifyTokenExpiry = undefined;
-    await user.save();
-
-    return NextResponse.json({
-      message: "Email verified successfully",
-      success: true,
-    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
